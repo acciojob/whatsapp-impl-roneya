8688ba99bb8db91851c144f31bc21cf2f6fbed15
Rohan Vidhate <49265832+roneya@users.noreply.github.com>
2023-02-02 20:05:40 +0530

    Update WhatsappService.java

--- src/main/java/com/driver/WhatsappService.java
+++ src/main/java/com/driver/WhatsappService.java
@@ -1,43 +1,29 @@
+
 package com.driver;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class WhatsappService {
-    
-    WhatsappRepository whatsappRepository = new WhatsappRepository();
+    WhatsappRepository repository = new WhatsappRepository();
 
     public String createUser(String name, String mobile) throws Exception {
-        return whatsappRepository.createUser(name, mobile);
+        return repository.createUser(name,mobile);
     }
-
     public Group createGroup(List<User> users){
-        return whatsappRepository.createGroup(users);
+        return repository.createGroup(users);
     }
-
+    public int sendMassageTogroup(Message message, User sender, Group group) throws Exception {
+        return repository.sendMassageToGroup(message,sender,group);
+    }
+    public String changeAdmin(User approver, User user, Group group) throws Exception {
+        return repository.changeAdmin(approver,user,group);
+    }
     public int createMessage(String content){
-        return whatsappRepository.createMessage(content);
-    }
-
-    public int sendMessage(Message message, User sender, Group group) throws Exception{
-
-        return  whatsappRepository.sendMessage(message, sender, group);
-    }
-
-    public String changeAdmin(User approver, User user, Group group) throws Exception{
-        return whatsappRepository.changeAdmin(approver, user, group);
-    }
-
-    public int removeUser(User user) throws Exception{
-
-        return whatsappRepository.removeUser(user);
-    }
-
-    public String findMessage(Date start, Date end, int K) throws Exception{
-
-        return whatsappRepository.findMessage(start, end, K);
+        return repository.createMessage(content);
     }
 }
